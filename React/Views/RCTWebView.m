e766cb6056c33fc110c31979a944c1892be98288
Your Name <you@example.com>
2020-05-04 15:52:53 +0900

    20200504 react-native iOS UIWebView forked

---
+++ React/Views/RCTWebView.m
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "RCTWebView.h"
+
+#import <WebKit/WebKit.h>
+
+#import "RCTAutoInsetsProtocol.h"
+#import "RCTConvert.h"
+#import "RCTEventDispatcher.h"
+#import "RCTLog.h"
+#import "RCTUtils.h"
+#import "RCTView.h"
+#import "UIView+React.h"
+
+NSString *const RCTJSNavigationScheme = @"react-js-navigation";
+
+static NSString *const kPostMessageHost = @"postMessage";
+
+@interface RCTWebView () <UIWebViewDelegate, RCTAutoInsetsProtocol>
+
+@property (nonatomic, copy) RCTDirectEventBlock onLoadingStart;
+@property (nonatomic, copy) RCTDirectEventBlock onLoadingFinish;
+@property (nonatomic, copy) RCTDirectEventBlock onLoadingError;
+@property (nonatomic, copy) RCTDirectEventBlock onShouldStartLoadWithRequest;
+@property (nonatomic, copy) RCTDirectEventBlock onMessage;
+
+@end
+
+@implementation RCTWebView
+{
+  WKWebView *_webView;
+  NSString *_injectedJavaScript;
+}
+
+- (void)dealloc
+{
+//  _webView.delegate = nil;
+}
+
+@end
